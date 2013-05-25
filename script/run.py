05e32bdb607261f347de154fc92f83383845a1e4
Taewook Oh <twoh@liberty-research.org>
2013-05-25 16:13:46 -0400

    add driver script

---
+++ script/run.py
@@ -0,0 +1,270 @@
+#!/usr/bin/python
+
+import argparse
+import sys
+import subprocess
+import random
+import re
+
+NO_INJECTION = 100
+
+injector = ''
+prog = ''
+cmp = ''
+ref = ''
+timeout = -1
+sampling_interval = 10000
+first_sample = 0
+outdir = ''
+max_process = 10
+
+def get_static_insts(prog):
+  insts = []
+
+  p = subprocess.Popen(['objdump', '-S', prog], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  out, err = p.communicate()
+
+  lines = out.split('\n')
+  for line in lines:
+    if re.match( r'^  [0-9a-f]*:\t', line, re.M):
+      addr = line.partition(':\t')[0].lstrip()
+      insts.append(addr)
+
+  return insts
+
+def get_buffer_addrs(prog):
+  p = subprocess.Popen(['objdump', '-D', prog], stdout=subprocess.PIPE)
+  out, err = p.communicate()
+
+  a = ''
+  o = ''
+  t = ''
+  for line in out.split('\n'):
+    if re.match( r'^[0-9a-f]+ <__after_bp>:', line, re.M):
+      a = line.split()[0]
+    if re.match( r'^[0-9a-f]+ <__org_inst>:', line, re.M):
+      o = line.split()[0]
+    if re.match( r'^[0-9a-f]+ <__target_dynamic_instance>:', line, re.M):
+      t = line.split()[0]
+   
+  return (a, o, t)
+
+def reserve_process_queue(process, glog):
+  # two pass
+  terminated = []
+  early_termination = False
+  for sp_tuple in process:
+    if (sp_tuple[0].poll() is None):
+      continue
+    else:
+      terminated.append(sp_tuple)
+      injected = postprocess(sp_tuple, glog)
+      if injected == False:
+        early_termination = True
+ 
+  # remove postprocessed processes from the list
+  for elem in terminated:
+    process.remove(elem)
+  if early_termination == True:
+    return True
+
+  while len(process) == max_process:
+    sp_tuple = process[0]
+    process.remove(sp_tuple)
+
+    if (sp_tuple[0].poll() is None):
+      process.append(sp_tuple)
+    else:
+      injected = postprocess(sp_tuple, glog)
+      return not injected
+
+  return False
+
+def cleanup(process, glog):
+  while len(process) != 0:
+    sp_tuple = process[0]
+    process.remove(sp_tuple)
+
+    if (sp_tuple[0].poll() is None):
+      process.append(sp_tuple)
+    else:
+      postprocess(sp_tuple, glog)
+
+def postprocess(sp_tuple, glog):
+  returncode = sp_tuple[0].returncode
+  breakpoint = sp_tuple[1]
+  target_invocation = sp_tuple[2]
+  stdout = sp_tuple[3]
+  stderr = sp_tuple[4]
+
+  log = stdout.name
+  out = stderr.name
+
+  stdout.close()
+  stderr.close()
+
+  # compare output
+  cmpout = open( outdir+'/cmp.%s.%d' % (breakpoint, target_invocation), 'w' )
+  p_cmp = subprocess.Popen([cmp, out, ref], stdout=cmpout, stderr=subprocess.PIPE)
+  cmpresult = p_cmp.wait()
+  cmpout.close()
+
+  # remove out
+  p_rm = subprocess.Popen(['rm', '-f', out], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+  p_rm.wait() 
+
+  glog.write('bp %s invocation %d exitcode %d cmpresult %d\n' % (breakpoint, target_invocation, returncode, cmpresult)) 
+
+  return returncode != NO_INJECTION
+
+def run(insts, after_bp_addr, org_inst_addr, target_addr, glog):
+  def get_args(inst, target_invocation):
+    args = []
+    args.append(injector)
+    args.append('--type')
+    args.append('a')
+    args.append('--prog')
+    args.append(prog)
+    args.append('--breakpoint')
+    args.append('0x%s' % inst)
+    args.append('--target')
+    args.append('%d' % target_invocation)
+    args.append('--timeout')
+    args.append('%d' % timeout)
+    args.append('--after_bp_addr')
+    args.append('0x%s' % after_bp_addr)
+    args.append('--org_inst_addr')
+    args.append('0x%s' % org_inst_addr)
+    args.append('--target_addr')
+    args.append('0x%s' % target_addr)
+    return args
+
+  # tuple of (Popen object, iteration, bptime)
+  process = []
+
+  index = 0
+  sample_count = 0
+  target_invocation = first_sample
+  while index < len(insts):
+    inst = insts[index]
+
+    shift = reserve_process_queue(process, glog)
+    if shift or sample_count == 10:
+      # shift to next instruction
+      cleanup(process, glog)
+      index += 1
+      sample_count = 0
+      target_invocation = first_sample
+      continue
+
+    # invoke process
+    args = get_args(inst, target_invocation)
+    outfile = outdir+'/output.%s.%d' % (inst,target_invocation)
+    logfile = outdir+'/log.%s.%d' % (inst,target_invocation)
+
+    f_stderr = open(outfile, 'w')
+    f_stdout = open(logfile, 'w')
+    sp = subprocess.Popen(args, stdout=f_stdout, stderr=f_stderr)
+    process.append( (sp, inst, target_invocation, f_stdout, f_stderr) )
+    
+    target_invocation += sampling_interval 
+    sample_count += 1
+
+def summarize(glog, summary):
+  pcs = []
+
+  masked = 0      # exitcode == 0, cmp == 0
+  sdc = 0         # exitcode == 0, cmp == 1
+  skipped = 0     # exitcode == 100
+  timeout= 0      # exitcode == 100
+  detected = 0    # exitcode == 102
+  crashed = 0     # else
+  total = 0
+
+  for line in glog.readlines():
+    total += 1
+    exitcode = line.partition('exitcode ')[2].partition(',')[0]
+    pc = line.partition('bp ')[2].partition(',')[0]
+    pcs.append(pc)
+    cmp = line.partition('cmpresult ')[2].partition(',')[0]
+
+    if (exitcode == '0'):
+      if (cmp == '0'):
+        masked += 1
+      elif (cmp == '1'):
+        sdc += 1
+      else:
+        summary.write('unsupported cmpresult %s appeared, line %s' % (cmp, line))
+        return
+    elif (exitcode == '100'):
+      skipped += 1
+    elif (exitcode == '101'):
+      timeout += 1
+    elif (exitcode == '102'):
+      detected += 1
+    else:
+      crashed += 1
+
+  summary.write('total: %d\n' % total)
+
+  masked_perc = '%.2f' % (float(masked)/float(total)*100)
+  summary.write('masked: %d(%s)\n' % ( masked, masked_perc ))
+
+  sdc_perc = '%.2f' % (float(sdc)/float(total)*100)
+  summary.write('sdc: %d(%s)\n' % ( sdc, sdc_perc ))
+
+  skipped_perc = '%.2f' % (float(skipped)/float(total)*100)
+  summary.write('skipped: %d(%s)\n' % ( skipped, skipped_perc ))
+
+  timeout_perc = '%.2f' % (float(timeout)/float(total)*100)
+  summary.write('timeout: %d(%s)\n' % ( timeout, timeout_perc ))
+
+  detected_perc = '%.2f' % (float(detected)/float(total)*100)
+  summary.write('detected: %d(%s)\n' % ( detected, detected_perc ))
+
+  crashed_perc = '%.2f' % (float(crashed)/float(total)*100)
+  summary.write('crashed: %d(%s)\n' % ( crashed, crashed_perc ))
+
+  pcs_set = set(pcs)
+  summary.write('static breakpoint: %d\n' % len( pcs_set ))
+
+  for bp in pcs_set:
+    summary.write('  %s : %d\n' % (bp, pcs.count(bp)))
+
+if __name__ == '__main__':
+  argparser = argparse.ArgumentParser(description='Injector driver')
+  argparser.add_argument('-i', '--injector', help='Injector program', required=True)
+  argparser.add_argument('-p', '--prog', help='Target program', required=True)
+  argparser.add_argument('-c', '--cmp', help='Program to compare outputs. Exit code should be 0 if outputs are same, non-zero otherwise', required=True)
+  argparser.add_argument('-r', '--ref', help='Reference output to check correctness', required=True)
+  argparser.add_argument('-t', '--timeout', type=int, help='Timeout', default=-1)
+  argparser.add_argument('-s', '--sampling_interval', type=int, help='Sampling interval', default=10000)
+  argparser.add_argument('-f', '--first_sample', type=int, help='Invocation count of first sample for each breakpoint', default=0)
+  argparser.add_argument('-o', '--outdir', help='Directory to put outputs', required=True)
+  argparser.add_argument('-m', '--max_process', type=int, help='Maximum number of parallel processes', default=10)
+  args = vars(argparser.parse_args())
+
+  # initialize global variables 
+  injector = args['injector']
+  prog = args['prog']
+  cmp = args['cmp']
+  ref = args['ref']
+  timeout = args['timeout']
+  sampling_interval = args['sampling_interval']
+  first_sample = args['first_sample']
+  outdir = args['outdir']
+  max_process = args['max_process']
+
+  insts = get_static_insts(prog)
+  (after_bp_addr, org_inst_addr, target_addr) = get_buffer_addrs(prog)
+
+  glog = open(outdir+'/glog.txt', 'w')
+  run(insts, after_bp_addr, org_inst_addr, target_addr, glog)
+  glog.close()
+
+  glog = open(outdir+'/glog.txt', 'r')
+  summary = open(outdir+'/summary.txt', 'w')
+  summarize(glog, summary)
+  summary.close()
+  glog.close()
+
